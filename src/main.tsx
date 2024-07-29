e85b59517250c5ab32f05f9f087b144721ddbbda
Daarkosss <karolpilat59@gmail.com>
2024-07-29 22:53:02 +0200

    Add i18next for translations

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./Keycloack.ts";
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import './i18n';
 import App from './App.tsx'
 import './scss/main.scss';
 
