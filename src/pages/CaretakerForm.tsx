288e72b058e8582434e3f40d87e8f69e72cd6c33
Daarkosss <karolpilat59@gmail.com>
2024-08-01 22:47:01 +0200

    Use react multidate picker to select availability

--- src/pages/CaretakerForm.tsx
+++ src/pages/CaretakerForm.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import DatePicker from 'react-datepicker';
 import { useDropzone } from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
 import { Header } from '../components/Header';
+import DatePicker, { Value } from 'react-multi-date-picker';
+import DatePanel from "react-multi-date-picker/plugins/date_panel"
+import weekends from "react-multi-date-picker/plugins/highlight_weekends"
 
 interface CaregiverFormProps {
   onSubmit: (formData: FormData) => void;
@@ -14,10 +16,9 @@ const CaretakerForm: React.FC<CaregiverFormProps> = ({ onSubmit }) => {
 
   const [location, setLocation] = useState('');
   const [animalTypes, setAnimalTypes] = useState<string[]>([]);
-  const [prices, setPrices] = useState<{ [key: string]: number }>({});
+  const [prices, setPrices] = useState<{ [key: string]: string }>({});
   const [description, setDescription] = useState('');
-  const [availabilityStart, setAvailabilityStart] = useState<Date | null>(null);
-  const [availabilityEnd, setAvailabilityEnd] = useState<Date | null>(null);
+  const [availability, setAvailability] = useState<Value[][]>();
   const [images, setImages] = useState<File[]>([]);
 
   const onDrop = (acceptedFiles: File[]) => {
@@ -28,34 +29,51 @@ const CaretakerForm: React.FC<CaregiverFormProps> = ({ onSubmit }) => {
 
   const handleAnimalTypeChange = (type: string) => {
     if (animalTypes.includes(type)) {
-      setAnimalTypes(animalTypes.filter(t => t !== type));
+      setAnimalTypes(animalTypes.filter((t) => t !== type));
     } else {
       setAnimalTypes([...animalTypes, type]);
     }
   };
 
-  const handlePriceChange = (type: string, price: number) => {
-    setPrices({ ...prices, [type]: price });
+  const handlePriceChange = (type: string, price: string) => {
+    if (/^\d*\.?\d{0,2}$/.test(price) || price === '') {
+      setPrices({ ...prices, [type]: price });
+    }
+  };
+
+  const validatePrices = () => {
+    let isValid = true;
+    const newErrors: { [key: string]: string } = {};
+
+    animalTypes.forEach((type) => {
+      if (!prices[type] || !/^\d+(\.\d{1,2})?$/.test(prices[type])) {
+        isValid = false;
+        newErrors[type] = t('caretakerForm.invalidPrice');
+      }
+    });
+
+    return isValid;
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('location', location);
-    formData.append('description', description);
-    if (availabilityStart) formData.append('availabilityStart', availabilityStart.toISOString());
-    if (availabilityEnd) formData.append('availabilityEnd', availabilityEnd.toISOString());
 
-    animalTypes.forEach(type => {
-      formData.append('animalTypes', type);
-      formData.append(`price_${type}`, prices[type].toString());
-    });
+    if (validatePrices()) {
+      const formData = new FormData();
+      formData.append('location', location);
+      formData.append('description', description);
 
-    images.forEach((image, index) => {
-      formData.append(`image_${index}`, image);
-    });
+      animalTypes.forEach((type) => {
+        formData.append('animalTypes', type);
+        formData.append(`price_${type}`, prices[type]);
+      });
 
-    onSubmit(formData);
+      images.forEach((image, index) => {
+        formData.append(`image_${index}`, image);
+      });
+
+      onSubmit(formData);
+    }
   };
 
   return (
@@ -63,10 +81,10 @@ const CaretakerForm: React.FC<CaregiverFormProps> = ({ onSubmit }) => {
       <Header />
       <div className='form-container'>
         <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="location" className='form-group'>
+          <Form.Group controlId='location' className='form-group'>
             <Form.Label className='form-label'>{t('caretakerForm.location')}</Form.Label>
             <Form.Control
-              type="text"
+              type='text'
               value={location}
               onChange={(e) => setLocation(e.target.value)}
               placeholder={t('caretakerForm.enterCity')}
@@ -78,65 +96,70 @@ const CaretakerForm: React.FC<CaregiverFormProps> = ({ onSubmit }) => {
             <Form.Label className='form-label'>{t('caretakerForm.animalTypes')}</Form.Label>
             <div className='animal-type'>
               <Form.Check
-                type="checkbox"
+                type='checkbox'
                 label={t('caretakerForm.smallDog')}
                 onChange={() => handleAnimalTypeChange('smallDog')}
                 checked={animalTypes.includes('smallDog')}
                 className='form-check'
               />
               {animalTypes.includes('smallDog') && (
-                <Form.Control
-                  type="number"
-                  pattern="/^\d+(\.\d{0,2})?$/"
-                  value={prices['smallDog'] || ''}
-                  onChange={(e) => handlePriceChange('smallDog', parseFloat(e.target.value))}
-                  placeholder={t('caretakerForm.priceFor', { type: t('caretakerForm.smallDog') })}
-                  className='form-control price-input'
-                />
+                <div className='price-container'>
+                  <Form.Control
+                    type='text'
+                    value={prices['smallDog'] || ''}
+                    onChange={(e) => handlePriceChange('smallDog', e.target.value)}
+                    placeholder={t('caretakerForm.priceFor', { type: t('caretakerForm.smallDog') })}
+                    className='form-control price-input'
+                  />
+                </div>
               )}
             </div>
             <div className='animal-type'>
               <Form.Check
-                type="checkbox"
+                type='checkbox'
                 label={t('caretakerForm.mediumDog')}
                 onChange={() => handleAnimalTypeChange('mediumDog')}
                 checked={animalTypes.includes('mediumDog')}
                 className='form-check'
               />
               {animalTypes.includes('mediumDog') && (
-                <Form.Control
-                  pattern="\d+(\.\d{2})?"
-                  value={prices['mediumDog'] || ''}
-                  onChange={(e) => handlePriceChange('mediumDog', parseFloat(e.target.value))}
-                  placeholder={t('caretakerForm.priceFor', { type: t('caretakerForm.mediumDog') })}
-                  className='form-control price-input'
-                />
+                <div className='price-container'>
+                  <Form.Control
+                    type='text'
+                    value={prices['mediumDog'] || ''}
+                    onChange={(e) => handlePriceChange('mediumDog', e.target.value)}
+                    placeholder={t('caretakerForm.priceFor', { type: t('caretakerForm.mediumDog') })}
+                    className='form-control price-input'
+                  />
+                </div>
               )}
             </div>
             <div className='animal-type'>
               <Form.Check
-                type="checkbox"
+                type='checkbox'
                 label={t('caretakerForm.cat')}
                 onChange={() => handleAnimalTypeChange('cat')}
                 checked={animalTypes.includes('cat')}
                 className='form-check'
               />
               {animalTypes.includes('cat') && (
-                <Form.Control
-                  type="number"
-                  value={prices['cat'] || ''}
-                  onChange={(e) => handlePriceChange('cat', parseFloat(e.target.value))}
-                  placeholder={t('caretakerForm.priceFor', { type: t('caretakerForm.cat') })}
-                  className='form-control price-input'
-                />
+                <div className='price-container'>
+                  <Form.Control
+                    type='text'
+                    value={prices['cat'] || ''}
+                    onChange={(e) => handlePriceChange('cat', e.target.value)}
+                    placeholder={t('caretakerForm.priceFor', { type: t('caretakerForm.cat') })}
+                    className='form-control price-input'
+                  />
+                </div>
               )}
             </div>
           </Form.Group>
 
-          <Form.Group controlId="description" className='form-group'>
+          <Form.Group controlId='description' className='form-group'>
             <Form.Label className='form-label'>{t('caretakerForm.description')}</Form.Label>
             <Form.Control
-              as="textarea"
+              as='textarea'
               rows={3}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
@@ -145,27 +168,22 @@ const CaretakerForm: React.FC<CaregiverFormProps> = ({ onSubmit }) => {
             />
           </Form.Group>
 
-          <Form.Group controlId="availability" className='form-group'>
+          <Form.Group controlId='availability' className='form-group'>
             <Form.Label className='form-label'>{t('caretakerForm.availabilityCalendar')}</Form.Label>
-            <div>
-              <Form.Label className='form-label'>{t('caretakerForm.startDate')}</Form.Label>
-              <DatePicker
-                selected={availabilityStart}
-                onChange={(date: Date | null) => setAvailabilityStart(date)}
-                className='form-control'
-              />
-            </div>
-            <div>
-              <Form.Label className='form-label'>{t('caretakerForm.endDate')}</Form.Label>
-              <DatePicker
-                selected={availabilityEnd}
-                onChange={(date: Date | null) => setAvailabilityEnd(date)}
-                className='form-control'
-              />
-            </div>
+            <DatePicker
+              value={availability}
+              onChange={setAvailability}
+              multiple
+              range
+              format='DD-MM-YYYY'
+              plugins={[
+                weekends(),
+                <DatePanel sort="date" style={{ width: 150 }} />
+              ]}
+            />
           </Form.Group>
 
-          <Form.Group controlId="images" className='form-group'>
+          <Form.Group controlId='images' className='form-group'>
             <Form.Label className='form-label'>{t('caretakerForm.uploadImages')}</Form.Label>
             <div {...getRootProps({ className: 'dropzone' })}>
               <input {...getInputProps()} />
@@ -178,7 +196,7 @@ const CaretakerForm: React.FC<CaregiverFormProps> = ({ onSubmit }) => {
             </div>
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant='primary' type='submit'>
             {t('caretakerForm.submit')}
           </Button>
         </Form>
