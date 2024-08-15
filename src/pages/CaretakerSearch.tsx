f38df5c16fa51365a447698db33ab36cf8a7af40
Daarkosss <karolpilat59@gmail.com>
2024-08-15 10:35:24 +0200

    Temporary disable eslint rule

--- src/pages/CaretakerSearch.tsx
+++ src/pages/CaretakerSearch.tsx
@@ -51,6 +51,7 @@ const CaretakerList = () => {
   
   useEffect(() => {
     fetchCaretakers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pagingParams]);
 
   const handleTableChange = (
