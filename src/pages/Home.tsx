3c62e72c1752f8f6d55978e4cb9b08f3885cc468
Jakub <kuba.staniszewski.ks@gmail.com>
2024-08-29 23:27:21 +0200

    navigate users that do not have selected_profile property selected to /profile-selection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 import { api } from "../api/api";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import store from "../store/RootStore";
 
 const Home = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const [message, setMessage] = useState<string>(t('loading'));
+  const [message, setMessage] = useState<string>(t("loading"));
 
   useEffect(() => {
-    handleGetMessage();
+    if (!store.user.profile?.selected_profile) {
+      navigate("/profile-selection");
+    } else {
+      handleGetMessage();
+    }
   }, []);
 
   const handleGetMessage = async () => {
@@ -19,21 +24,31 @@ const Home = () => {
       const response = await api.getTestMessage();
       setMessage(response);
     } catch (error) {
-      setMessage('Failed to fetch message');
+      setMessage("Failed to fetch message");
     }
-  }
-  
+  };
+
   return (
-    <div>
-      <Header />
-      <div className="home-container">
-        <h1>{t('home.title')} {message}</h1>
-        <Button variant="outline-dark" onClick={handleGetMessage}>{t('home.requestMessage')}</Button>
-        <Button variant="outline-dark" onClick={() => api.getXsrfToken()}>{t('home.fetchToken')}</Button>
-        <Button variant="dark" onClick={() => navigate('/caretaker/form')}>{t('home.changeCaretakerForm')}</Button>
+    store.user.profile?.selected_profile && (
+      <div>
+        <Header />
+        <div className="home-container">
+          <h1>
+            {t("home.title")} {message}
+          </h1>
+          <Button variant="outline-dark" onClick={handleGetMessage}>
+            {t("home.requestMessage")}
+          </Button>
+          <Button variant="outline-dark" onClick={() => api.getXsrfToken()}>
+            {t("home.fetchToken")}
+          </Button>
+          <Button variant="dark" onClick={() => navigate("/caretaker/form")}>
+            {t("home.changeCaretakerForm")}
+          </Button>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  );
+};
 
 export default Home;
