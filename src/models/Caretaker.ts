133a6d3726bf3fbbefdedd724d054b10228ff193
Daarkosss <karolpilat59@gmail.com>
2024-09-14 19:49:22 +0200

    Create models: Caretaker and Voivodeship

---
+++ src/models/Caretaker.ts
@@ -0,0 +1,41 @@
+import { CaretakerDTO, OfferDTO } from '../types';
+import Voivodeship from './Voivodeship';
+
+class Caretaker {
+  accountData: {
+    email: string;
+    name: string;
+    surname: string;
+  };
+  phoneNumber: string;
+  description: string;
+  address: {
+    city: string;
+    zipCode: string;
+    voivodeship: Voivodeship;
+    street: string;
+    buildingNumber: string;
+    apartmentNumber: string;
+  };
+  animals: string[];
+  offers: OfferDTO[];
+  numberOfRatings: number;
+  avgRating: number | null;
+
+  constructor(data: CaretakerDTO) {
+    this.accountData = data.accountData;
+    this.phoneNumber = data.phoneNumber;
+    this.description = data.description;
+    this.address = {
+      ...data.address,
+      voivodeship: new Voivodeship(data.address.voivodeship),
+    };
+    this.animals = data.animals;
+    this.offers = data.offers;
+    this.numberOfRatings = data.numberOfRatings;
+    this.avgRating = data.avgRating;
+  }
+}
+  
+export default Caretaker;
+  
---
+++ src/models/Voivodeship.ts
@@ -0,0 +1,38 @@
+import { VoivodeshipDTO } from '../types';
+
+const voivodeshipMap: Record<VoivodeshipDTO, string> = {
+  DOLNOSLASKIE: 'dolnośląskie',
+  KUJAWSKO_POMORSKIE: 'kujawsko-pomorskie',
+  LUBELSKIE: 'lubelskie',
+  LUBUSKIE: 'lubuskie',
+  LODZKIE: 'łódzkie',
+  MALOPOLSKIE: 'małopolskie',
+  MAZOWIECKIE: 'mazowieckie',
+  OPOLSKIE: 'opolskie',
+  PODKARPACKIE: 'podkarpackie',
+  PODLASKIE: 'podlaskie',
+  POMORSKIE: 'pomorskie',
+  SLASKIE: 'śląskie',
+  SWIETOKRZYSKIE: 'świętokrzyskie',
+  WARMINSKO_MAZURSKIE: 'warmińsko-mazurskie',
+  WIELKOPOLSKIE: 'wielkopolskie',
+  ZACHODNIOPOMORSKIE: 'zachodniopomorskie',
+};
+
+class Voivodeship {
+  private name: VoivodeshipDTO;
+
+  constructor(name: VoivodeshipDTO) {
+    this.name = name;
+  }
+
+  get originalName(): VoivodeshipDTO {
+    return this.name;
+  }
+
+  toString = () : string => {
+    return voivodeshipMap[this.name];
+  }
+}
+
+export default Voivodeship;
