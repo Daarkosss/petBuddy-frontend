1bee53eca497a218c4c2b52eb7f6a70a50e0c060
Jakub <kuba.staniszewski.ks@gmail.com>
2024-09-19 10:19:52 +0200

    commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
-import { toast } from 'react-toastify';
-import store from '../store/RootStore';
+import { toast } from "react-toastify";
+import store from "../store/RootStore";
 
-const backendHost = import.meta.env.VITE_BACKEND_HOST || window.location.hostname;
-const backendPort = import.meta.env.VITE_BACKEND_PORT || '8081';
+const backendHost =
+  import.meta.env.VITE_BACKEND_HOST || window.location.hostname;
+const backendPort = import.meta.env.VITE_BACKEND_PORT || "8081";
 export const PATH_PREFIX = `http://${backendHost}:${backendPort}/`;
 
-type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
+type Method = "GET" | "POST" | "PUT" | "DELETE";
 
 export interface User {
   _id: string;
@@ -14,31 +15,29 @@ export interface User {
 }
 
 class API {
-
   async fetch<T>(
     method: Method,
     path: string,
     body?: unknown,
-    headers: HeadersInit = {},
+    headers: HeadersInit = {}
   ): Promise<T> {
-    
     const options = {
       method,
       headers: {
-        'Content-Type': 'application/json',
-        'X-XSRF-TOKEN': store.user.xsrfToken,
-        ...headers
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": store.user.xsrfToken,
+        ...headers,
       },
       body: body ? JSON.stringify(body) : undefined,
-      credentials: 'include'
-    } as RequestInit
+      credentials: "include",
+    } as RequestInit;
 
-    const response = await fetch(`${PATH_PREFIX}${path}`, options)
+    const response = await fetch(`${PATH_PREFIX}${path}`, options);
     const data = await response.json();
 
     if (!response.ok) {
-      toast.error(data.message || 'Wrong server response!');
-      throw new Error(data.message || 'Wrong server response!');
+      toast.error(data.message || "Wrong server response!");
+      throw new Error(data.message || "Wrong server response!");
     } else {
       return data;
     }
@@ -50,39 +49,30 @@ class API {
     body?: unknown
   ): Promise<T> {
     if (store.user.jwtToken) {
-      return this.fetch<T>(
-        method,
-        path,
-        body,
-        { 'Authorization': `Bearer ${store.user.jwtToken}` },
-      );
+      return this.fetch<T>(method, path, body, {
+        Authorization: `Bearer ${store.user.jwtToken}`,
+      });
     } else {
-      return Promise.reject(new Error('No user token available'));
+      return Promise.reject(new Error("No user token available"));
     }
   }
 
   async getTestMessage(): Promise<string> {
     try {
-      console.log('getting test message')
-      const response = await this.authorizedFetch<string>(
-        'GET',
-        'api/test'
-      )
+      console.log("getting test message");
+      const response = await this.authorizedFetch<string>("GET", "api/test");
       toast.success(JSON.stringify(response));
-      return response
-    } catch(error: unknown) {
-      return "brak"
+      return response;
+    } catch (error: unknown) {
+      return "brak";
     }
   }
 
   async getXsrfToken(): Promise<void> {
     try {
-      const response = await this.fetch<void>(
-        'GET',
-        'api/csrf'
-      )
-      return response
-    } catch(error: unknown) {
+      const response = await this.fetch<void>("GET", "api/csrf");
+      return response;
+    } catch (error: unknown) {
       return;
     }
   }
