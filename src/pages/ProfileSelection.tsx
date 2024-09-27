28c27fd20e11ce52b449bebad21241b20cbea27e
Jakub <kuba.staniszewski.ks@gmail.com>
2024-09-27 19:23:56 +0200

    Show appropriate profile selection options based on available user profiles

--- src/pages/ProfileSelection.tsx
+++ src/pages/ProfileSelection.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../scss/pages/_profileSelection.scss";
 import { useTranslation } from "react-i18next";
 import { IoAddCircleOutline } from "react-icons/io5";
+import { LuHelpingHand } from "react-icons/lu";
 import ProfileSelectionBox from "../components/ProfileSelectionBox";
 import { CgProfile } from "react-icons/cg";
 import { Header } from "../components/Header";
 import RoundedLine from "../components/RoundedLine";
+import { api } from "../api/api";
 
 const ProfileSelection: React.FC = () => {
   const { t } = useTranslation();
 
-  // useEffect(() => {
-  //     store.reset();
-  //   }, []);
+  const [hasCaretakerProfile, setHasCaretakerProfile] = useState(false);
+  const [isRequesting, setIsRequesting] = useState(true);
+
+  useEffect(() => {
+    const getUserProfiles = async () => {
+      try {
+        const userProfiles = await api.getUserProfiles();
+        if ("hasCaretakerProfile" in userProfiles) {
+          setHasCaretakerProfile(userProfiles.hasCaretakerProfile);
+          setIsRequesting(false);
+        }
+      } catch (error) {
+        console.log("Failed to fetch user profiles data");
+      }
+    };
+
+    getUserProfiles();
+  }, []);
 
   return (
     <>
@@ -20,22 +37,32 @@ const ProfileSelection: React.FC = () => {
       <div className="selection-main-container">
         <h1 className="selection-title">{t("profileSelection.title")}</h1>
         <RoundedLine width="40%" height="5px" backgroundColor="#007EA7" />
-        <div className="selection-nested-container">
-          <ProfileSelectionBox
-            icon={CgProfile}
-            size={100}
-            title={t("profileSelection.client")}
-            profile={"Client"}
-          />
 
-          {/*TODO: remember to add logic that allows to add caretaker profile if needed!*/}
-          <ProfileSelectionBox
-            icon={IoAddCircleOutline}
-            size={100}
-            title={t("profileSelection.createCaretaker")}
-            profile={null}
-          />
-        </div>
+        {!isRequesting && (
+          <div className="selection-nested-container">
+            <ProfileSelectionBox
+              icon={CgProfile}
+              size={100}
+              title={t("profileSelection.client")}
+              profile={"Client"}
+            />
+            {hasCaretakerProfile === true ? (
+              <ProfileSelectionBox
+                icon={LuHelpingHand}
+                size={100}
+                title={t("caretaker")}
+                profile={"Caretaker"}
+              />
+            ) : (
+              <ProfileSelectionBox
+                icon={IoAddCircleOutline}
+                size={100}
+                title={t("profileSelection.createCaretaker")}
+                profile={null}
+              />
+            )}
+          </div>
+        )}
       </div>
     </>
   );
