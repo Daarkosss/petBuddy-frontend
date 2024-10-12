71faa2dc561c4a81b2b62179e09db43b826ad805
Jakub <kuba.staniszewski.ks@gmail.com>
2024-10-12 14:41:16 +0200

    create chat messages

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import Message from "./Message";
+import "../scss/components/_chatMessages.scss";
 
-function ChatMessages() {
-  return <div></div>;
+interface ChatBoxContent {
+  messages: {
+    id: number;
+    chatId: number;
+    senderEmail: string;
+    content: string;
+    createdAt: string;
+    seenByRecipient: boolean;
+  }[];
 }
+
+const ChatMessages: React.FC<ChatBoxContent> = ({ messages }) => {
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  useEffect(scrollToBottom, [messages]);
+
+  return (
+    <div className="chat-messages-container">
+      {messages.map((message, i) => (
+        <div key={i}>
+          <Message message={message.content} name={message.senderEmail} />
+        </div>
+      ))}
+      <div ref={messagesEndRef} />
+    </div>
+  );
+};
 
 export default ChatMessages;
