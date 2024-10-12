946c655e63df68a986d38070184207d93da02eea
Jakub <kuba.staniszewski.ks@gmail.com>
2024-10-12 22:59:50 +0200

    check if chat with specified user exists and if not initialize it

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,10 +1,161 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatTopBar from "./ChatTopBar";
 import ChatMessages from "./ChatMessages";
 import ChatBottom from "./ChatBottom";
 import "../scss/components/_chatBox.scss";
+import { Client, IFrame, Stomp } from "@stomp/stompjs";
+import sockjs from "sockjs-client/dist/sockjs";
+import store from "../store/RootStore";
+import { api } from "../api/api";
+import { ChatRoom } from "../types/chat.types";
 
-function ChatBox() {
+interface ChatBoxProperties {
+  recipientEmail: string;
+}
+
+const ChatBox: React.FC<ChatBoxProperties> = ({ recipientEmail }) => {
+  // const principalUsername = "user@backend.com";
+  const chatId = 1; // chatId == 1 already exists on backend and user@backend.com is a member of this chat
+
+  const [wsClient, setWsClient] = useState<Client | null>(null);
+  const [message, setMessage] = useState<string>("");
+  const [sessionId, setSessionId] = useState<string | undefined>();
+  const [inputMessage, setInputMessage] = useState<string>("");
+  const [doesChatRoomExist, setDoesChatRoomExist] = useState<boolean | null>(
+    null
+  );
+  const [chatRoomData, setChatRoomData] = useState<ChatRoom | null>(null);
+
+  //TODO: is it ok to create it in ChatBox?
+  useEffect(() => {
+    initWebsocketConnection();
+    return () => {
+      disconnectWebSocket();
+    };
+  }, []);
+
+  useEffect(() => {
+    subscribeToSession();
+  }, [wsClient]);
+
+  useEffect(() => {
+    checkIfChatRoomExists();
+  }, [wsClient, store.user.profile]);
+
+  useEffect(() => {
+    subscribeToChatRoom();
+  }, [sessionId]);
+
+  //TODO: implement this
+  const checkIfChatRoomExists = async () => {
+    try {
+      const data = await api.getChatRoomWithGivenUser(
+        recipientEmail,
+        "Europe/Warsaw"
+      );
+      console.log(`dane: ${data}`);
+      setChatRoomData(data);
+      setDoesChatRoomExist(true);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        const regex = /Status code: ([0-9]{3})/;
+        const match = error.message.match(regex);
+        if (match != null && match[1] === "404") {
+          setDoesChatRoomExist(false);
+        }
+        console.log(`error message: ${error.message}, match: ${match![1]}`);
+      }
+    }
+  };
+
+  //TODO: timezone
+  const initializeChatRoom = (message: string) => {
+    api.initializeChatRoom(recipientEmail, message, "Europe/Warsaw");
+  };
+
+  const initWebsocketConnection = () => {
+    const socket = new sockjs(
+      `http://localhost:8081/ws?token=${store.user.jwtToken}`
+    );
+    const client = Stomp.over(() => socket);
+    const onConnect = (frame: IFrame) => {
+      setWsClient(client);
+    };
+    client.connect({}, onConnect);
+  };
+
+  const subscribeToSession = () => {
+    if (wsClient === null) {
+      return;
+    }
+
+    wsClient.subscribe(
+      `/topic/session/${store.user.profile?.email}`,
+      (message) => {
+        const parsedMessage = JSON.parse(message.body);
+        console.log("SESSION ID", parsedMessage.sessionId);
+        setSessionId(parsedMessage.sessionId);
+      }
+    );
+  };
+
+  const subscribeToChatRoom = () => {
+    if (sessionId === undefined || wsClient === null) {
+      return;
+    }
+    // Accept-Role - required header
+    // Accept-Timezone - optional header (caches initial timezone in the backend)
+    const headers = {
+      "Accept-Role": "CLIENT",
+      "Accept-Timezone": "Europe/Warsaw",
+    };
+    wsClient.subscribe(
+      `/topic/messages/${chatId}/${sessionId}`,
+      (message) => {
+        console.log("Join message", message.body);
+        setMessage(message.body);
+      },
+      headers
+    );
+  };
+
+  const sendMessageToChatRoom = () => {
+    // Accept-Role - required header
+    // Accept-Timezone - optional header (overrides cached timezone in the backend)
+    const headers = {
+      "Accept-Role": "CLIENT",
+      "Accept-Timezone": "Asia/Tokyo",
+    };
+    if (wsClient && inputMessage && sessionId) {
+      const chatMessage = { content: inputMessage };
+      wsClient.publish({
+        destination: `/app/chat/${chatId}`,
+        body: JSON.stringify(chatMessage),
+        headers: headers,
+      });
+      setInputMessage("");
+    }
+  };
+
+  const disconnectWebSocket = () => {
+    if (wsClient) {
+      wsClient.deactivate().then(() => {
+        console.log("Disconnected");
+      });
+    }
+  };
+
+  const getCookieNative = (name: string) => {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+      const [key, value] = cookie.split("=");
+      if (key === name) {
+        return decodeURIComponent(value);
+      }
+    }
+    return null;
+  };
+
   const mockupMessages = [
     {
       id: 1,
@@ -26,17 +177,20 @@ function ChatBox() {
 
   const onSend = (input: string) => {
     console.log(input);
+    initializeChatRoom(input);
   };
 
   return (
     <div className="chat-box-main-container">
-      <div className="chat-box-inner-container">
-        <ChatTopBar />
-        <ChatMessages messages={mockupMessages} />
-        <ChatBottom onSend={onSend} />
-      </div>
+      {doesChatRoomExist != null && (
+        <div className="chat-box-inner-container">
+          <ChatTopBar />
+          <ChatMessages messages={mockupMessages} />
+          <ChatBottom onSend={onSend} />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default ChatBox;
