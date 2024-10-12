87f9bce3f6281ff66b1c5e8313aa4873d45ca09a
Jakub <kuba.staniszewski.ks@gmail.com>
2024-10-12 14:41:02 +0200

    create chat bottom

--- src/components/ChatBottom.tsx
+++ src/components/ChatBottom.tsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 import "../scss/components/_chatBottom.scss";
 
-function ChatBottom() {
-  const [input, setInput] = useState<string>("");
+interface ChatBottomParameters {
+  onSend: (input: string) => any;
+}
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+const ChatBottom: React.FC<ChatBottomParameters> = ({ onSend }) => {
+  const [input, setInput] = useState<string>("");
 
   return (
     <div className="chat-bottom-container">
       <div>opcje</div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <button type="submit">{">"}</button>
-      </form>
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button onClick={() => onSend(input)}>{">"}</button>
     </div>
   );
-}
+};
 
 export default ChatBottom;
