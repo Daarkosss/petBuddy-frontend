db0b1ea327234cc2cca2b366dace7d333e533e9d
Jakub <kuba.staniszewski.ks@gmail.com>
2024-10-12 14:40:32 +0200

    create initial ChatBox

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -5,12 +5,35 @@ import ChatBottom from "./ChatBottom";
 import "../scss/components/_chatBox.scss";
 
 function ChatBox() {
+  const mockupMessages = [
+    {
+      id: 1,
+      chatId: 1,
+      senderEmail: "kuba.staniszewski.ks@gmail.com",
+      content: "Hello",
+      createdAt: "2024-10-12T11:44:26.078Z",
+      seenByRecipient: true,
+    },
+    {
+      id: 2,
+      chatId: 1,
+      senderEmail: "test@gmail.com",
+      content: "Hello there!",
+      createdAt: "2024-10-12T11:45:26.078Z",
+      seenByRecipient: true,
+    },
+  ];
+
+  const onSend = (input: string) => {
+    console.log(input);
+  };
+
   return (
     <div className="chat-box-main-container">
       <div className="chat-box-inner-container">
         <ChatTopBar />
-        <ChatMessages />
-        <ChatBottom />
+        <ChatMessages messages={mockupMessages} />
+        <ChatBottom onSend={onSend} />
       </div>
     </div>
   );
