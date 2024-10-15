a4887ea42e71d75c7dc37761c7c1d76d67281939
Jakub <kuba.staniszewski.ks@gmail.com>
2024-10-15 16:19:48 +0200

    add more chat types

--- src/types/chat.types.ts
+++ src/types/chat.types.ts
@@ -1,3 +1,5 @@
+import { PageableDTO, SortDTO } from "./pagination.types";
+
 export type ChatRoom = {
   id: number;
   chatterEmail: string;
@@ -7,4 +9,32 @@ export type ChatRoom = {
   lastMessage: string;
   lastMessageSendBy: string;
   seenByPrincipal: boolean;
+};
+
+export type ChatMessage = {
+  id: number;
+  chatId: number;
+  senderEmail: string;
+  content: string;
+  createdAt: string;
+  seenByRecipient: boolean;
+};
+
+export type ChatMessagesResponse = {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: ChatMessage[];
+  number: number;
+  sort: SortDTO;
+  first: boolean;
+  last: boolean;
+  numberOfElements: number;
+  pageable: PageableDTO;
+  empty: boolean;
+};
+
+export type WebsocketResponse = {
+  type: string;
+  content: ChatMessage;
 };
