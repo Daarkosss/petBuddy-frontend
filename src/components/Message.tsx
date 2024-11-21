0c5f7d4234e53ba6a49671b08564d9588ee57c5e
Jakub <kuba.staniszewski.ks@gmail.com>
2024-11-22 00:53:29 +0100

    style message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import store from "../store/RootStore";
 import "../scss/components/_message.scss";
+import { Avatar } from "antd";
 
 interface MessageContent {
   message: string;
-  name: string;
+  recipientName: string;
+  recipientSurname: string;
+  senderEmail: string;
+  timeSent: string;
+  showAvatar: boolean;
 }
 
-const Message: React.FC<MessageContent> = ({ message, name }) => {
+const Message: React.FC<MessageContent> = ({
+  message,
+  recipientName,
+  recipientSurname,
+  senderEmail,
+  timeSent,
+  showAvatar,
+}) => {
+  const [showDate, setShowDate] = useState<boolean>(false);
   return (
     <div
       className={
-        store.user.profile?.email === name
+        store.user.profile?.email === senderEmail
           ? "message-author"
           : "message-recipient"
       }
+      onClick={() => setShowDate(!showDate)}
     >
-      <div>
-        <div>{message}</div>
-        <div>{name}</div>
+      <div className="message-cloud-avatar">
+        <div className="message-content-name">
+          <div className="message-content">{message}</div>
+          <div className="message-sender">
+            {store.user.profile?.email === senderEmail
+              ? `${store.user.profile?.firstName} ${store.user.profile?.lastName}`
+              : `${recipientName} ${recipientSurname}`}
+          </div>
+        </div>
+        <div className="message-avatar-container">
+          <Avatar
+            size={15}
+            style={{
+              visibility: showAvatar === true ? "visible" : "hidden",
+            }}
+          />
+        </div>
+      </div>
+
+      <div
+        className="message-send-time"
+        style={{ display: showDate === true ? "inline" : "none" }}
+      >
+        {`${new Date(timeSent).toLocaleDateString()} ${new Date(
+          timeSent
+        ).toLocaleTimeString()}`}
       </div>
     </div>
   );
