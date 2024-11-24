e25c2adf9247981c6e2289f6b539d9b44c480d1a
Jakub <kuba.staniszewski.ks@gmail.com>
2024-11-24 21:26:19 +0100

    handle chat notifications

--- src/api/NotificationWebSocket.ts
+++ src/api/NotificationWebSocket.ts
@@ -5,16 +5,20 @@ import store from "../store/RootStore";
 import { PATH_PREFIX } from "./api";
 import { toast } from "react-toastify";
 import { Notification } from "../types/notification.types";
+import { action, makeAutoObservable } from "mobx";
 
 class NotificationWebSocket {
   private wsClient: Client | null = null;
+  receiveNewMessageTrigger: boolean = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 
   initWebsocketConnection = () => {
     this.disconnectWebSocket();
 
-    const socket = new sockjs(
-      `${PATH_PREFIX}ws?token=${store.user.jwtToken}`
-    );
+    const socket = new sockjs(`${PATH_PREFIX}ws?token=${store.user.jwtToken}`);
     const client = Stomp.over(() => socket);
     const onConnect = () => {
       this.wsClient = client;
@@ -23,21 +27,34 @@ class NotificationWebSocket {
     client.connect({}, onConnect);
   };
 
+  changeReceiveMessagetriggerValue = () => {
+    //to trigger chatBadge load chat rooms with newest data
+    this.receiveNewMessageTrigger = !this.receiveNewMessageTrigger;
+  };
+
   subscribeToSession = () => {
     if (!this.wsClient || !store.user.profile?.selected_profile) {
       return;
     }
 
-    this.wsClient.subscribe(
-      "/user/topic/notification",
-      (message) => {
-        const newNotification: Notification = JSON.parse(message.body);
-        if (newNotification.receiverProfile === store.user.profile?.selected_profile) {
-          store.notification.addNotification(newNotification);
-          toast.info(i18next.t("notification.newNotification"));
-        } 
-      },
-    );
+    this.wsClient.subscribe("/user/topic/notification", (message) => {
+      const newNotification = JSON.parse(message.body);
+      console.log(`Nowa wiadomość ${JSON.stringify(newNotification)} dd`);
+      if (
+        newNotification.receiverProfile === store.user.profile?.selected_profile
+      ) {
+        store.notification.addNotification(newNotification);
+        toast.info(i18next.t("notification.newNotification"));
+      }
+
+      if (newNotification.dType === "CHAT_NOTIFICATION") {
+        this.changeReceiveMessagetriggerValue();
+        store.notification.setunreadChats(
+          newNotification.unseenChatsAsClient,
+          newNotification.unseenChatsAsCaretaker
+        );
+      }
+    });
   };
 
   disconnectWebSocket = () => {
--- src/store/NotificationStore.tsx
+++ src/store/NotificationStore.tsx
@@ -1,6 +1,7 @@
 import { makeAutoObservable } from "mobx";
 import { api } from "../api/api";
 import { Notification } from "../types/notification.types";
+import store from "../store/RootStore";
 
 class NotificationStore {
   unread: Notification[] = [];
@@ -23,7 +24,7 @@ class NotificationStore {
       if (notifications) {
         this.unread = notifications.content;
       }
-    } catch(error) {
+    } catch (error) {
       console.error("Error while fetching notifications");
     }
   }
@@ -34,13 +35,29 @@ class NotificationStore {
       if (numberOfUnreadChats !== undefined) {
         this.unreadChats = numberOfUnreadChats;
       }
-    } catch(error) {
+    } catch (error) {
       console.error("Error while fetching number of unread chats");
     }
   }
 
+  setunreadChats(unseenChatsAsClient: number, unseenChatsAsCaretaker: number) {
+    if (store.user.profile?.selected_profile === "CARETAKER") {
+      this.unreadChats = unseenChatsAsCaretaker;
+    }
+
+    if (store.user.profile?.selected_profile === "CLIENT") {
+      console.log(
+        `setting unseen chat messages as client to: ${unseenChatsAsClient}`
+      );
+      this.unreadChats = unseenChatsAsClient;
+    }
+  }
+
   addNotification(notification: Notification) {
-    if (notification.objectType === "CARE" && notification.objectId === this.openCareId) {
+    if (
+      notification.objectType === "CARE" &&
+      notification.objectId === this.openCareId
+    ) {
       location.reload();
     } else {
       this.unread.unshift(notification);
@@ -50,9 +67,9 @@ class NotificationStore {
   async markAllAsRead() {
     try {
       await api.markAllNotificationsAsRead();
-      this.reset()
-    } catch(error) {
-      console.error("Error")
+      this.reset();
+    } catch (error) {
+      console.error("Error");
     }
   }
 
@@ -64,27 +81,38 @@ class NotificationStore {
           try {
             return await api.markNotificationAsRead(notifId);
           } catch (error) {
-            console.error(`Failed to mark notification ${notifId} as read`, error);
+            console.error(
+              `Failed to mark notification ${notifId} as read`,
+              error
+            );
             return null;
           }
         })
       );
 
-      this.removeNotifications(deletedNotifications.map((notif) => notif!.notificationId));
+      this.removeNotifications(
+        deletedNotifications.map((notif) => notif!.notificationId)
+      );
     } catch (error) {
-      throw new Error("Something went wrong while marking notifications as read");
+      throw new Error(
+        "Something went wrong while marking notifications as read"
+      );
     } finally {
       console.log(this.unread.length);
     }
   }
 
   removeNotifications(notificationIds: number[]) {
-    this.unread = this.unread.filter((notif) => !notificationIds.includes(notif.notificationId));
+    this.unread = this.unread.filter(
+      (notif) => !notificationIds.includes(notif.notificationId)
+    );
   }
-  
+
   getCareNotifications(careId: number) {
     return this.unread
-      .filter((notif) => notif.objectType === "CARE" && notif.objectId === careId)
+      .filter(
+        (notif) => notif.objectType === "CARE" && notif.objectId === careId
+      )
       .map((notif) => notif.notificationId);
   }
 
