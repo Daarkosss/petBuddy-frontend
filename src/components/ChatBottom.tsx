a26cb067559aaa15efd44a5b4ea96780a1e54407
Jakub <kuba.staniszewski.ks@gmail.com>
2024-11-24 21:21:04 +0100

    allow to send using enter, erase input, add styling to chatBottom

--- src/components/ChatBottom.tsx
+++ src/components/ChatBottom.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "../scss/components/_chatBottom.scss";
+import { Button, Input } from "antd";
+import { MoreOutlined, SendOutlined } from "@ant-design/icons";
+import "../scss/components/_buttons.scss";
 
 interface ChatBottomParameters {
   onSend: (input: string) => any;
@@ -8,15 +11,34 @@ interface ChatBottomParameters {
 const ChatBottom: React.FC<ChatBottomParameters> = ({ onSend }) => {
   const [input, setInput] = useState<string>("");
 
+  const onKeyDown = (e: string) => {
+    if (e !== undefined) {
+      if (e === "Enter") {
+        onSend(input);
+        setInput("");
+      }
+    }
+  };
+
   return (
     <div className="chat-bottom-container">
-      <div>opcje</div>
-      <input
-        type="text"
-        value={input}
+      <MoreOutlined size={90} />
+      <Input
+        placeholder="Write message..."
         onChange={(event) => setInput(event.target.value)}
+        onKeyDown={(e) => onKeyDown(e.code)}
+        value={input}
       />
-      <button onClick={() => onSend(input)}>{">"}</button>
+      <Button
+        type="primary"
+        className="send-message-button"
+        onClick={() => {
+          setInput("");
+          onSend(input);
+        }}
+      >
+        <SendOutlined />
+      </Button>
     </div>
   );
 };
