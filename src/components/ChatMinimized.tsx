3824a9e840c54bde416deea4c4c8a67c8abc0972
Jakub <kuba.staniszewski.ks@gmail.com>
2024-11-24 21:41:42 +0100

    add chat minimized styling

--- src/components/ChatMinimized.tsx
+++ src/components/ChatMinimized.tsx
@@ -1,32 +1,46 @@
-import { BorderOutlined } from "@ant-design/icons";
+import { BorderOutlined, CloseOutlined, MenuOutlined } from "@ant-design/icons";
 import React from "react";
-import "../scss/components/_chatTopBar.scss";
+import "../scss/components/_chatMinimized.scss";
 
 interface MinimizedChatProps {
   name: string;
   surname: string;
   profile: string;
+  onClose: Function;
+  onMaximize: Function;
 }
 
 const ChatMinimized: React.FC<MinimizedChatProps> = ({
   name,
   surname,
   profile,
+  onClose,
+  onMaximize,
 }) => {
   return (
-    <div className="chat-top-bar-container">
-      <div className="options-top-big-container">
-        <div className="options-top-small-container">
-          <BorderOutlined />
-        </div>
-      </div>
-      <div className="comment-container-top">
-        <div className="comment-container-user-rating">
-          <div className="comment-container-user-nick-container">
-            <h5 className="comment-container-user-nick">
-              {name} {surname}
-            </h5>
-            <p>{profile}</p>
+    <div className="chat-minimized-box-main-container-wrap">
+      <div className="chat-minimized-box-main-container">
+        <div className="chat-minimized-box-inner-container">
+          <div className="chat-top-bar-container">
+            <div className="options-top-big-container">
+              <div className="options-top-small-container-left">
+                <MenuOutlined />
+              </div>
+              <div className="options-top-small-container-right">
+                <BorderOutlined onClick={() => onMaximize()} />
+                <CloseOutlined onClick={() => onClose()} />
+              </div>
+            </div>
+            <div className="comment-container-top">
+              <div className="comment-container-user-rating">
+                <div className="comment-container-user-nick-container">
+                  <h5 className="comment-container-user-nick">
+                    {name} {surname}
+                  </h5>
+                  <p>{profile}</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
