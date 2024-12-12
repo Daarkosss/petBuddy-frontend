e2e7fe06cd40d2caa7a617151bad1c1c19d69d8b
Jakub <kuba.staniszewski.ks@gmail.com>
2024-12-12 01:35:41 +0100

    add translations and, temporary dummy button

--- src/pages/CaretakerProfile.tsx
+++ src/pages/CaretakerProfile.tsx
@@ -417,6 +417,19 @@ const CaretakerProfile: React.FC<CaretakerProfileProps> = observer(
               </div>
               <div>
                 <h2>{t("profilePage.userCaretakerProfile")}</h2>
+                {isMyProfile === true && (
+                  <div>
+                    <Button
+                      type="primary"
+                      className="profile-action-button"
+                      onClick={() => {
+                        // navigate("/caretaker/form");
+                      }}
+                    >
+                      {t("profilePage.viewFinancialStats")}
+                    </Button>
+                  </div>
+                )}
                 <RoundedLine
                   width={"100%"}
                   height={"2px"}
